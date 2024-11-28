7b88d6d7ab6bfb443ce31798c3a084334827ce0b
Luis Ramirez <146735904+LuisRamirezv@users.noreply.github.com>
2024-11-28 17:08:12 +0000

    adding the income gross to the user class

--- src/n/Admin.java
+++ src/n/Admin.java
@@ -7,6 +7,74 @@
  *
  * @author telmuun
  */
-public class Admin extends User {
+public class Admin extends User{
+
+    public Admin(String firstName, String lastName, String birthDate, String email, int phoneNumber) {
+        super(firstName, lastName, birthDate, email, phoneNumber);
+    }
+
+    @Override
+    public void setPhoneNumber(int phoneNumber) {
+        
+    }
+
+    @Override
+    public void setUserID(int userID) {
+        super.setUserID(userID); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void setBirthDate(String birthDate) {
+        super.setBirthDate(birthDate); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void setName(String lastName) {
+        super.setName(lastName); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        super.setFirstName(firstName); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public int getUserID() {
+        return super.getUserID(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public int getPhoneNumber() {
+        return super.getPhoneNumber(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String email() {
+        return super.email(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String getBirthDate() {
+        return super.getBirthDate(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    @Override
+    public String getFirstName() {
+        return super.getFirstName(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+
+ 
+    
     
 }
