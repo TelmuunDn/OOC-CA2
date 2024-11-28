88c714a8c49e7c4b1984b3e32b4205fab75de593
Luis Ramirez <146735904+LuisRamirezv@users.noreply.github.com>
2024-11-28 15:31:39 +0000

    adding class files to work

---
+++ src/n/welcmome.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author telmuun
+ */
+public class welcmome {
+    public static void main(String[] args){
+        System.out.println("Enter your name");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        String name = scanner.nextLine();
+        
+        scanner.close();
+        
+        System.out.println("Hello, " + name);
+        
+    
+    
+    
+    }
+    
+    
+}
